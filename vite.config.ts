23da76b8d4377872d4b228c726de0bd5034377a6
kakaeve <zkzkdlqm@gmail.com>
2024-01-23 15:08:32 +0900

    Init: absolute path setting 절대경로 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import second from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [
+      { find: '@components', replacement: '/src/components' },
+      { find: '@', replacement: '/src' },
+    ],
+  },
 })
