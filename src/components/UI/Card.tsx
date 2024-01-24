b0099b3fd669369621bbd09504ffbdbc3e2ee2dd
kakaeve <zkzkdlqm@gmail.com>
2024-01-24 14:50:42 +0900

    Fix: Card component add className props Card component에 className 속성이 없어서 나는 오류 발견 후 수정

--- src/components/UI/Card.tsx
+++ src/components/UI/Card.tsx
@@ -4,10 +4,11 @@ import classes from './Card.module.css'
 
 type CardProps = {
   children: React.ReactNode
+  className: string
 }
 
 const Card = (props: CardProps) => {
-  return <div className={classes.card}>{props.children}</div>
+  return <div className={`${classes.card} ${props.className}`}>{props.children}</div>
 }
 
 export default Card
